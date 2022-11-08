bd3702d5fc4fa9beeae1512bda0cbd8cab3bf9a4
jiii-9 <succeed.g9@gmail.com>
2022-11-08 22:30:37 +0900

    save the input value in state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 function ExpenseForm() {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
   const titleChangeHandler = event => {
-    console.log(event);
+    setEnteredTitle(event.target.value);
+  };
+
+  const amountChangeHandler = event => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = event => {
+    setEnteredDate(event.target.value);
   };
 
   return (
@@ -17,11 +29,21 @@ function ExpenseForm() {
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="100" step="100" />
+          <input
+            type="number"
+            min="100"
+            step="100"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2020-01-01" max="2023-12-31" />
+          <input
+            type="date"
+            min="2020-01-01"
+            max="2023-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
 
