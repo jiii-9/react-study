71dc3311a5ebcca767d391c105c691352c965025
jiii-9 <succeed.g9@gmail.com>
2022-11-08 23:01:31 +0900

    different ways to store state value

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,38 +2,41 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 function ExpenseForm() {
-  // const [enteredTitle, setEnteredTitle] = useState("");
-  // const [enteredAmount, setEnteredAmount] = useState("");
-  // const [enteredDate, setEnteredDate] = useState("");
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    enteredAmount: "",
-    enteredDate: "",
-  });
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
 
   const titleChangeHandler = event => {
-    // setEnteredTitle(event.target.value);
-    setUserInput({
-      ...userInput, // 바뀌지 않는 값은 그대로 복사해줘야한다.
-      enteredTitle: event.target.value, // 바뀌는 부분만 새로 저장해준다.
-    });
+    setEnteredTitle(event.target.value);
+    // setUserInput({
+    //   ...userInput, // 바뀌지 않는 값은 그대로 복사해줘야한다.
+    //   enteredTitle: event.target.value, // 바뀌는 부분만 새로 저장해준다.
+    // });
+    // setUserInput((prevState) => {
+    //   return {...prevState, enteredTitle: event.target.value}
+    // })
   };
 
   const amountChangeHandler = event => {
-    // setEnteredAmount(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredAmount: event.target.value,
-    });
+    setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
   };
 
   const dateChangeHandler = event => {
-    // setEnteredDate(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredDate: event.target.value,
-    });
+    setEnteredDate(event.target.value);
+    // setUserInput({
+    // ...userInput,
+    // enteredDate: event.target.value,
+    // });
   };
 
   return (
